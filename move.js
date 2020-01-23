61fe1382c104d15147c62a0228081a9c795e1d9b
Zruiry <zruiry@qq.com>
2020-01-23 18:10:54 +0800

    修复bug，增加链式运动

--- move.js
+++ move.js
@@ -1,11 +1,21 @@
-function move (obj,name,tgt) {
-				clearInterval(obj.timer)
-				obj.timer = setInterval(function () {
-					if (parseInt(getComputedStyle(obj)[name]) == tgt) {
-						clearInterval(obj.timer)
-					} else{
-						let speed = (tgt - parseInt(getComputedStyle(obj)[name])) / 10
-						obj.style[name] = parseInt(getComputedStyle(obj)[name]) + speed + 'px'
-					}
-				},30)
+function move(obj, name, tgt, fn) {
+	clearInterval(obj.timer)
+	obj.timer = setInterval(function() {
+		let speed = (tgt - parseInt(getComputedStyle(obj)[name])) / 10
+		if (speed > 0) {
+			speed = Math.ceil(speed)
+		}
+		if (speed < 0) {
+			speed = Math.floor(speed)
+		}
+
+		if (parseInt(getComputedStyle(obj)[name]) == tgt) {
+			clearInterval(obj.timer)
+			if (fn) {
+				fn()
 			}
+		} else {
+			obj.style[name] = parseInt(getComputedStyle(obj)[name]) + speed + 'px'
+		}
+	}, 30)
+}
